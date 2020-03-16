04b4dbca345cbe9f86bf6ff4dccc6594d37779d8
Max24S <48694869+Max24S@users.noreply.github.com>
2020-03-16 20:25:28 +0300

    Update item.php

--- item.php
+++ item.php
@@ -1,9 +1,9 @@
 <?php
-require_once('models/users.class.php');
+require_once('models/news.class.php');
 
 //$Users = new Users();
 //$news = $Users->get_all();
-$NewsClass = new Users();
+$NewsClass = new News();
 $id = $_GET['id'];
 $news = $NewsClass->get_one(['id'=>$id]);
 if(!$news){
@@ -14,11 +14,14 @@ if(!$news){
 ?>
 
 <div>
-    <?php echo $news['id']?>
+    id:<?php echo $news['id']?>
+    name:<?php echo $news['name']?>
+    date:<?php echo $news['date_published']?>
+    status:<?php echo $news['status']?>
+    category:<?php echo $news['category']?>
+    text:<?php echo $news['text']?>
+    author:<?php echo $news['author']?>
 </div>
 <td>
     <a href="/edit.php?id=<?php echo $news['id']?>">edit</a>
 </td>
-<div>
-    <?php echo $news['date_published']?>
-</div>
